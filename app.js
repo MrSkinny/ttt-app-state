e5fac13da45d936f6900b0bb147eb98821021af8
Marina Dargitz <marina.dargitz@gmail.com>
2018-04-09 16:02:08 -0600

    Restructure and rewrite functions as expressions

--- app.js
+++ app.js
@@ -14,74 +14,47 @@ const state = {
   hits: 1
 };
 
-// State modification functions
-const startNewGame = () => {
-  state.cell1 = '.';
-  state.cell2 = '.';
-  state.cell3 = '.';
-  state.cell4 = '.';
-  state.cell5 = '.';
-  state.cell6 = '.';
-  state.cell7 = '.';
-  state.cell8 = '.';
-  state.cell9 = '.';
-  state.hits = 1;
-};
+//EVALUATE WIN
 
-const clickACell = (cellID) => {
-  console.log(cellID);
-  
-  const prop = 'cell' + cellID;
-  if (state[prop] === '.' && state.hits % 2 === 0){
-    state[prop] = 'o';
-    state.hits += 1;
-  }
-  else if (state[prop] === '.' && state.hits % 2 !== 0){
-    state[prop] = 'x';
-    state.hits += 1;
-  }   
-  
-  renderBoard(); 
-};
-
-// Render functions
-
+//update DOM
 const showWin = (first, second, third) => {
-  console.log('the winners: ' + first + second + third);
   $('body').find(`#${first}`).addClass('win');
   $('body').find(`#${second}`).addClass('win');
   $('body').find(`#${third}`).addClass('win');
+  //no other moves
+  // $('body').find('.board').off();
 
 };
-//Evaluate win
+//evaluate win state
 const evalWin = () => {
   if (state.cell1 === state.cell2 && state.cell3 === state.cell2 && state.cell2 !== '.'){
     showWin(1,2,3);
   }
-  if (state.cell1 === state.cell4 && state.cell7 === state.cell4 && state.cell4 !== '.'){
+  else if (state.cell1 === state.cell4 && state.cell7 === state.cell4 && state.cell4 !== '.'){
     showWin(1,4,7);
   }
-  if (state.cell1 === state.cell5 && state.cell9 === state.cell5 && state.cell5 !== '.'){
+  else if (state.cell1 === state.cell5 && state.cell9 === state.cell5 && state.cell5 !== '.'){
     showWin(1,5,9);
   }
-  if (state.cell2 === state.cell5 && state.cell8 === state.cell5 && state.cell5 !== '.'){
+  else if (state.cell2 === state.cell5 && state.cell8 === state.cell5 && state.cell5 !== '.'){
     showWin(2,5,8);
   }
-  if (state.cell3 === state.cell5 && state.cell7 === state.cell5 && state.cell5 !== '.'){
+  else if (state.cell3 === state.cell5 && state.cell7 === state.cell5 && state.cell5 !== '.'){
     showWin(3,5,7);
   }
-  if (state.cell3 === state.cell6 && state.cell9 === state.cell6 && state.cell6 !== '.'){
+  else if (state.cell3 === state.cell6 && state.cell9 === state.cell6 && state.cell6 !== '.'){
     showWin(3,6,9);
   }
-  if (state.cell4 === state.cell5 && state.cell6 === state.cell5 && state.cell5 !== '.'){
+  else if (state.cell4 === state.cell5 && state.cell6 === state.cell5 && state.cell5 !== '.'){
     showWin(4,5,6);
   }
-  if (state.cell7 === state.cell8 && state.cell9 === state.cell8 && state.cell8 !== '.'){
+  else if (state.cell7 === state.cell8 && state.cell9 === state.cell8 && state.cell8 !== '.'){
     showWin(7,8,9);
   }
   else return;
 };
 
+//RENDER BOARD
 const generateCell = (id) => {
   const cellID = 'cell' + id;
   const cellState = state[cellID];
@@ -113,30 +86,59 @@ const renderBoard = () => {
   
   // publish to DOM
   $('.board').html(board);
+  //add event handlers
   //evaluate any win conditions
   evalWin();
+  console.log('render board ran');
 };
 
 
+// State modification functions
+const startNewGame = () => {
+  state.cell1 = '.';
+  state.cell2 = '.';
+  state.cell3 = '.';
+  state.cell4 = '.';
+  state.cell5 = '.';
+  state.cell6 = '.';
+  state.cell7 = '.';
+  state.cell8 = '.';
+  state.cell9 = '.';
+  state.hits = 1;
+};
+
+const clickACell = (cellID) => {  
+  const prop = 'cell' + cellID;
+  if (state[prop] === '.' && state.hits % 2 === 0){
+    state[prop] = 'o';
+    state.hits += 1;
+  }
+  else if (state[prop] === '.' && state.hits % 2 !== 0){
+    state[prop] = 'x';
+    state.hits += 1;
+  }   
+  renderBoard(); 
+};
+
 // Event Listeners
-function handleCellClick () {
+const handleCellClick = () => {
   $('.board').on('click','.cell', function(){
-  //which cell clicked
+    //which cell clicked
     const cellID = (this.id);
     //invoke state modification
     clickACell(cellID);
     //invoke render function
     renderBoard();
   });
-}
-function handleNewGame () {
+};
+const handleNewGame = () => {
   $('#new-game').on('click', function(){
-  //reset state
+    //reset state
     startNewGame();
     //invoke render function
     renderBoard();
   });
-}
+};
 
 const main = () => {
   renderBoard();
