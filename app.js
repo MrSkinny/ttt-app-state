d2baee0a56d8fb641a1d88a66f0f58fbdcde2b9f
Marina Dargitz <marina.dargitz@gmail.com>
2018-04-09 10:27:21 -0600

    Create state object and set up render, listener functions

--- app.js
+++ app.js
@@ -1,8 +1,72 @@
+/* global $ */
+'use strict';
+
 const state = {
+  cell1 : {
+    x : false,
+    y : false
+  },
+  cell2 : {
+    x : false,
+    y : false
+  },
+  cell3 : {
+    x : false,
+    y : false
+  },
+  cell4 : {
+    x : false,
+    y : false
+  },
+  cell5 : {
+    x : false,
+    y : false
+  },
+  cell6 : {
+    x : false,
+    y : false
+  },
+  cell7 : {
+    x : false,
+    y : false
+  },
+  cell8 : {
+    x : false,
+    y : false
+  },
+  cell9 : {
+    x : false,
+    y : false
+  }
 };
 
 // State modification functions
+const startNewGame = () => {
+
+};
+
+const clickACell = () => {
+
+};
 
 // Render functions
+const renderBoard = () => {
 
+};
 // Event Listeners
+
+$('.board').on('click','.cell', function(event){
+  //which cell clicked
+
+  //invoke state modification
+
+  //invoke render function
+});
+
+$('#new-game').on('click', function(event){
+  //which cell clicked
+
+  //invoke state modification
+
+  //invoke render function
+});
