aeacb0cddb7c5b026df02d52be18c68e08a59756
Marina Dargitz <marina.dargitz@gmail.com>
2018-04-09 14:47:34 -0600

    implement EvaluateWin function

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ const startNewGame = () => {
 };
 
 const clickACell = (cellID) => {
+  console.log(cellID);
+  
   const prop = 'cell' + cellID;
   if (state[prop] === '.' && state.hits % 2 === 0){
     state[prop] = 'o';
@@ -41,6 +43,39 @@ const clickACell = (cellID) => {
 };
 
 // Render functions
+
+const showWin = (first, second, third) => {
+  console.log('the winners: ' + first + second + third);
+};
+//Evaluate win
+const evalWin = () => {
+  if (state.cell1 === state.cell2 && state.cell3 === state.cell2 && state.cell2 !== '.'){
+    showWin(1,2,3);
+  }
+  if (state.cell1 === state.cell4 && state.cell7 === state.cell4 && state.cell4 !== '.'){
+    showWin(1,4,7);
+  }
+  if (state.cell1 === state.cell5 && state.cell9 === state.cell5 && state.cell5 !== '.'){
+    showWin(1,5,9);
+  }
+  if (state.cell2 === state.cell5 && state.cell8 === state.cell5 && state.cell5 !== '.'){
+    showWin(2,5,8);
+  }
+  if (state.cell3 === state.cell5 && state.cell7 === state.cell5 && state.cell5 !== '.'){
+    showWin(3,5,7);
+  }
+  if (state.cell3 === state.cell6 && state.cell9 === state.cell6 && state.cell6 !== '.'){
+    showWin(3,6,9);
+  }
+  if (state.cell4 === state.cell5 && state.cell6 === state.cell5 && state.cell5 !== '.'){
+    showWin(4,5,6);
+  }
+  if (state.cell7 === state.cell8 && state.cell9 === state.cell8 && state.cell8 !== '.'){
+    showWin(7,8,9);
+  }
+  else return;
+};
+
 const generateCell = (id) => {
   const cellID = 'cell' + id;
   const cellState = state[cellID];
@@ -70,16 +105,19 @@ const renderBoard = () => {
   let board = row1 + row2 + row3;
   // publish to DOM
   $('.board').html(board);
-
+  
 };
+
+
 // Event Listeners
 function handleCellClick () {
-  $('.board').on('click','.cell', function(event){
+  $('.board').on('click','.cell', function(){
   //which cell clicked
     const cellID = (this.id);
     //invoke state modification
     clickACell(cellID);
     //invoke render function
+    evalWin();
     renderBoard();
   });
 }
@@ -96,8 +134,6 @@ const main = () => {
   renderBoard();
   handleCellClick();
   handleNewGame();
-
-
 };
 
 $(main);
